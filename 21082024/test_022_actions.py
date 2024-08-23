db131b2fc2bccd35aa4813cf2c1f4fa8be6e08ff
Rohit Kalshetty <krohit7545@gmail.com>
2024-08-23 18:21:34 +0530

    Action, Windows

---
+++ 21082024/test_022_actions.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium.webdriver.common.action_chains import ActionChains, ActionBuilder
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.actions.mouse_button import MouseButton
+
+
+def test_022_actions():
+    driver = webdriver.Chrome()
+    driver.get("https://awesomeqa.com/selenium/mouse_interaction.html")
+
+    atag = driver.find_element(By.ID, "click")
+    atag.click()
+
+    time.sleep(2)
+    # Actions Builders - Mouse interactions
+
+    actions_builders = ActionBuilder(driver)
+    actions_builders.pointer_action.pointer_up(MouseButton.BACK)
+    actions_builders.perform()
+
+    time.sleep(10)
+    driver.quit()
+
